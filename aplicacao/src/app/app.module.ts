89e5e060bf5721842e48d23bb3f86284c3a9e7e6
lucas <lucasmaciel6690@gmail.com>
2021-10-14 19:33:45 -0300

    ções: Auto stash before checking out "origin/main"

--- aplicacao/src/app/app.module.ts
+++ aplicacao/src/app/app.module.ts
@@ -1,18 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { ROUTES } from './app-routes';
+import { HttpModule } from '@angular/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { HomeComponent } from './pages/home/home.component';
+import { MenuComponent } from './pages/menu/menu.component';
+import { RodapeComponent } from './pages/rodape/rodape.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    MenuComponent,
+    RodapeComponent
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(ROUTES),
+    HttpModule,
+    ReactiveFormsModule,
+    CommonModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [{ provide: LOCALE_ID, useValue: 'pt-BR'}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ aplicacao/src/app/model/login.model.ts
@@ -0,0 +1,7 @@
+export class Login {
+    constructor(
+        public usuario: number,
+        public senha: string,
+    ){}
+
+}
