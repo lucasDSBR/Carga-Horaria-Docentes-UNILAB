d4aeed02bd98171a3654c9b48aeb790c596018e1
lucas <lucasmaciel6690@gmail.com>
2021-10-15 10:21:52 -0300

    Implementação

--- aplicacao/src/app/app-routes.ts
+++ aplicacao/src/app/app-routes.ts
@@ -1,9 +1,13 @@
 import { Routes } from "@angular/router";
 
+import { AutenticacaoGuard } from './services/autenticacaoGuard.service';
+
 import { LoginComponent } from "./pages/login/login.component";
 import { HomeComponent } from "./pages/home/home.component";
 
 export const ROUTES: Routes = [
-    {path: "", component: LoginComponent}
+    {path: "", component: LoginComponent},
+    {path: "inicio", component: HomeComponent, canActivate: [ AutenticacaoGuard ]}
+    
 
 ]
