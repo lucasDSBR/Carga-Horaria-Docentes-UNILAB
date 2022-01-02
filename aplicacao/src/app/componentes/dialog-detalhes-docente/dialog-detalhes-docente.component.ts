10c25c8a669a56fb01acd2386d6fa5e03ea9a8d1
lucas <lucasmaciel6690@gmail.com>
2022-01-02 17:09:10 -0300

    modificacoes

--- aplicacao/src/app/componentes/dialog-detalhes-docente/dialog-detalhes-docente.component.ts
+++ aplicacao/src/app/componentes/dialog-detalhes-docente/dialog-detalhes-docente.component.ts
@@ -1,19 +1,23 @@
-import {Component, OnInit, Inject} from '@angular/core';
+import {Component, OnInit, Inject, ElementRef, ViewChild} from '@angular/core';
 
 import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
 
 import { InstitutoService } from '../../services/instituto.service';
 import { Institutos } from '../../model/institutos.model';
+import {ExcelService} from '../../services/excel.service';
+import jsPDF from 'jspdf'
+import autoTable from 'jspdf-autotable'
 
 @Component({
   selector: 'app-dialog-detalhes-docente',
   templateUrl: './dialog-detalhes-docente.component.html',
   styleUrls: ['./dialog-detalhes-docente.component.css'],
-  providers: [InstitutoService]
+  providers: [InstitutoService, ExcelService]
 })
 export class DialogDetalhesDocenteComponent implements OnInit {
   public dataUser: Array<Institutos>
   constructor(
+    private excelService:ExcelService,
     private institutoService: InstitutoService,
     public dialogRef: MatDialogRef<DialogDetalhesDocenteComponent>,
     @Inject(MAT_DIALOG_DATA) public data: {nomeDocente: string, instituto: string}
@@ -58,10 +62,46 @@ export class DialogDetalhesDocenteComponent implements OnInit {
       hiddenElement.href = 'data:text/csv;charset=utf-8,%EF%BB%BF' + encodeURI(csv);
       hiddenElement.target = '_blank';
       hiddenElement.download = this.data.nomeDocente+'.csv';
-      hiddenElement.click();
+      hiddenElement.baseURI
     }else{
 
     }
   }
+  exportAsXLSX():void {
+    this.excelService.exportAsExcelFile(this.dataUser, this.dataUser[0].docente);
+  }
+
+  captureScreen()  
+  {  
+    var doc = new jsPDF('l', 'pt');
+    var rows = [];
+    const head = [[' -- ','Ano Edital', 'Edital', 'Processo', 'Cotas', 'Projeto', 'Carga Horária', 'Função', 'UF', 'Instituto', 'Início do projeto', 'Fim do projeto']]
+    for(var key in this.dataUser){
+        var temp = [
+          key, 
+          this.dataUser[key].AnoEdital, 
+          this.dataUser[key].edital,
+          this.dataUser[key].processo,
+          this.dataUser[key].cotas,
+          this.dataUser[key].projeto,
+          this.dataUser[key].ch,
+          this.dataUser[key].funcao,
+          this.dataUser[key].Campus,
+          this.dataUser[key].Instituto,
+          this.dataUser[key].inicio_projeto,
+          this.dataUser[key].final_projeto
+        ];
+        rows.push(temp)
+    }
+
+
+    doc.text("Docente: "+this.data.nomeDocente, 10, 30)
+    autoTable(doc, {
+      head: head,
+      body: rows,
+      didDrawCell: (data) => { },
+    });
+    doc.save(this.data.nomeDocente+'.pdf');
+  }  
 
 }
